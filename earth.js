cb15119343e0dae0e1b34a8f05169e485da34440
qianlinjun <1025610469@qq.com>
2021-11-22 21:32:32 +0800

    suport rotate earth

--- earth.js
+++ earth.js
@@ -56,6 +56,12 @@ var FSHADER_SOURCE = "" +
 var canvas = document.getElementById("canvas");
 var gl = getWebGLContext(canvas);
 
+var indices_len;
+var texture;
+var u_Sampler;
+var image;
+var rotateAdd=0;
+
 
 function main() {
   if (!gl) {
@@ -69,19 +75,20 @@ function main() {
       return;
   }
 
-  initinfo = initVertexBuffer();
-  console.log(initinfo);
-
-
-  //设置视角矩阵的相关信息
-  var u_ModelViewMatrix = gl.getUniformLocation(gl.program, "u_ModelViewMatrix");
-  if (u_ModelViewMatrix < 0) {
-      console.log("无法获取矩阵变量的存储位置");
-      return;
+  if (initVertexBuffer() == true){
+    //进入场景初始化
+    drawTextures();
   }
 
-  //进入场景初始化
-  draw(gl, initinfo["indices_length"], initinfo["vertex_length"], u_ModelViewMatrix);
+
+  // //设置视角矩阵的相关信息
+  // var u_ModelViewMatrix = gl.getUniformLocation(gl.program, "u_ModelViewMatrix");
+  // if (u_ModelViewMatrix < 0) {
+  //     console.log("无法获取矩阵变量的存储位置");
+  //     return;
+  // }
+
+  
 
 }
 
@@ -89,7 +96,7 @@ function main() {
 
 function initVertexBuffer(){
     let r = 8;
-    let Bands  = 40;//纬度带
+    let Bands  = 80;//纬度带
     // let longitudeBands = 10;//经度带
     let positions_ = [];//存储x，y，z坐标
     let indices_ = [];//三角形列表（索引值）
@@ -163,152 +170,87 @@ function initVertexBuffer(){
     //绑定缓冲区对象并写入数据
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, position, gl.STATIC_DRAW);
-      //获取数组中一个元素所占的字节数
-      var fsize = position.BYTES_PER_ELEMENT;
-      //获取attribute -> a_Position变量的存储地址
-      var a_Position = gl.getAttribLocation(gl.program, "VertexPosition");
-      if (a_Position < 0) {
-          console.log("无法获取顶点位置的存储变量");
-          return -1;
-      }
-      
-      //对位置的顶点数据进行分配，并开启
-      var numComponents = 3;
-      var strideToNextPieceOfData = fsize*3;
-      var offsetIntoBuffer = 0;
-
-      gl.vertexAttribPointer(a_Position, numComponents, gl.FLOAT, false, strideToNextPieceOfData, offsetIntoBuffer);
-      gl.enableVertexAttribArray(a_Position);
-      /*************************************************/
-
-
-      /*************************************************/
-      // Write the normals to their buffer object.
-      gl.bindBuffer(gl.ARRAY_BUFFER, vertexNormalBuffer);
-      gl.bufferData(gl.ARRAY_BUFFER, normalData, gl.STATIC_DRAW);
-      // Assign normal to attrib and enable it.
-      let VertexNormal = gl.getAttribLocation(gl.program, 'VertexNormal');
-      gl.vertexAttribPointer(VertexNormal, 3, gl.FLOAT, false, 0, 0);
-      gl.enableVertexAttribArray(VertexNormal);
-      /*************************************************/
-
-
-
-
-      
-       /*************************************************/
-      // Write the normals to their buffer object.
-      gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
-      gl.bufferData(gl.ARRAY_BUFFER, textureCoord, gl.STATIC_DRAW);
-      // Assign texture coord to attrib and enable it.
-      var fsize1 = textureCoord.BYTES_PER_ELEMENT;
-      let a_TexCoord = gl.getAttribLocation(gl.program, 'a_TexCoord');
-      if(a_TexCoord < 0){
-          console.log("无法获取到存储位置");
-          return;
-      }
-      gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, fsize1 * 2, 0);
-      gl.enableVertexAttribArray(a_TexCoord);
-       /*************************************************/
-
-
-
-      //将顶点索引数据写入缓冲区对象
-      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-      gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-
-
-      return {"indices_length":indices.length, "vertex_length":textureCoord.length/2};
-}
-
-
-
-function draw(gl, indices_length, vertex_len, u_ModelViewMatrix, pos_len) {
-    var viewMatrix = new Matrix4();
-    // (视点，观察目标点，上方向)
-    //  setLookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ)
-    //664
-    viewMatrix.setLookAt(12,3,7,-0.5,0,0,0,1,0);///12,3,7,-0.5,0,0,0,1,0
-
-    var modelMatrix = new Matrix4();
-    // modelMatrix.setRotate(0, 0, 0, 1);
-    //设置模型矩阵的相关信息
-    // var modelMatrix = new Matrix4();
-    // modelMatrix.setRotate(0, 0, 5, 1);
-    // modelMatrix.setTranslate(1, 0, 0);
-    //console.log(modelMatrix);
-
-    //设置透视投影矩阵
-    var projMatrix = new Matrix4();
-    projMatrix.setPerspective(20, canvas.width/canvas.height, 1, 1000.0);
-
-    //计算出模型视图矩阵 viewMatrix.multiply(modelMatrix)相当于在着色器里面u_ViewMatrix * u_ModelMatrix
-    var modeViewProjectMatrix = projMatrix.multiply(viewMatrix.multiply(modelMatrix));
-
-
-
-
-    // Pass the modelView matrix into the shader.
-    let ModelViewMatrix = gl.getUniformLocation(gl.program, 'ModelViewMatrix');
-    gl.uniformMatrix4fv(ModelViewMatrix, false, viewMatrix.elements);
-
-    // Pass the mvp matrix into the shader.
-    let MVP = gl.getUniformLocation(gl.program, 'MVP');
-    gl.uniformMatrix4fv(MVP, false, modeViewProjectMatrix.elements);
-
-    // Pass the light position into the shader.
-    let LightPosition = gl.getUniformLocation(gl.program, 'LightPosition');
-    gl.uniform4fv(LightPosition, [-50.0, 50.0, 50.0, 1.0]);
-
-    // 材质漫反射颜色参数 传到着色器
-    let Kd = gl.getUniformLocation(gl.program, 'Kd');
-    gl.uniform3fv(Kd, [1.0, 1.0, 1.0]);
-
-    // 光照漫反射参数
-    let Ld = gl.getUniformLocation(gl.program, 'Ld');
-    gl.uniform3fv(Ld, [1.0, 1.0, 1.0]);
-
-
-
-
-    // Clear & draw.
-    // gl.clearColor(0.3, 0.3, 0.3, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    //获取数组中一个元素所占的字节数
+    var fsize = position.BYTES_PER_ELEMENT;
+    //获取attribute -> a_Position变量的存储地址
+    var a_Position = gl.getAttribLocation(gl.program, "VertexPosition");
+    if (a_Position < 0) {
+        console.log("无法获取顶点位置的存储变量");
+        return -1;
+    }
     
-    // gl.drawArrays(gl.LINE_STRIP,0, n/6);
+    //对位置的顶点数据进行分配，并开启
+    var numComponents = 3;
+    var strideToNextPieceOfData = fsize*3;
+    var offsetIntoBuffer = 0;
 
-    //配置纹理
-    if(!drawTextures(gl, indices_length)){
-      console.log("无法配置纹理");
-      return;
-  }
-  
-  // gl.drawElements(gl.TRIANGLES, indices_length, gl.UNSIGNED_SHORT, 0);
+    gl.vertexAttribPointer(a_Position, numComponents, gl.FLOAT, false, strideToNextPieceOfData, offsetIntoBuffer);
+    gl.enableVertexAttribArray(a_Position);
+    /*************************************************/
+
+
+    /*************************************************/
+    // Write the normals to their buffer object.
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexNormalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, normalData, gl.STATIC_DRAW);
+    // Assign normal to attrib and enable it.
+    let VertexNormal = gl.getAttribLocation(gl.program, 'VertexNormal');
+    gl.vertexAttribPointer(VertexNormal, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(VertexNormal);
+    /*************************************************/
+
+
+
+
+    
+      /*************************************************/
+    // Write the normals to their buffer object.
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, textureCoord, gl.STATIC_DRAW);
+    // Assign texture coord to attrib and enable it.
+    var fsize1 = textureCoord.BYTES_PER_ELEMENT;
+    let a_TexCoord = gl.getAttribLocation(gl.program, 'a_TexCoord');
+    if(a_TexCoord < 0){
+        console.log("无法获取到存储位置");
+        return;
+    }
+    gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, fsize1 * 2, 0);
+    gl.enableVertexAttribArray(a_TexCoord);
+      /*************************************************/
+
+
+
+    //将顶点索引数据写入缓冲区对象
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+    indices_len = indices.length
+
+    return true;
 }
-
 
 
     /*第四部分 initTextures() 创建纹理对象 并调用纹理绘制方法*/
-  function drawTextures(gl, n) {
-      var texture = gl.createTexture();//创建纹理对象
+  function drawTextures() {
+      texture = gl.createTexture();//创建纹理对象
       if(!texture){
           console.log("无法创建纹理对象");
           return;
       }
 
       //获取u_Sampler的存储位置
-      var u_Sampler = gl.getUniformLocation(gl.program,"u_Sampler");
+      u_Sampler = gl.getUniformLocation(gl.program,"u_Sampler");
       if(u_Sampler < 0){
           console.log("无法获取变量的存储位置");
           return;
       }
 
       //创建Image对象，并绑定加载完成事件
-      var image = new Image();
+      image = new Image();
       image.src = "./global.png";
 
       image.onload = function () {
-          loadTexture(n, texture, u_Sampler,image);
+          loadTexture();
       };
 
       
@@ -316,7 +258,9 @@ function draw(gl, indices_length, vertex_len, u_ModelViewMatrix, pos_len) {
   }
 
   /*第五部分 设置纹理相关信息供WebGL使用，并进行绘制*/
-  function loadTexture(n, texture, u_Sampler,image) {
+  function loadTexture() {
+
+    /********************************配置纹理参数***************************/
       //对纹理图像进行y轴反转
       gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
       //开启0号纹理单元
@@ -330,8 +274,6 @@ function draw(gl, indices_length, vertex_len, u_ModelViewMatrix, pos_len) {
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 
-
-
       //配置纹理图像 jpg格式对应gl.RGB
       gl.texImage2D(gl.TEXTURE_2D,0,gl.RGBA,gl.RGBA,gl.UNSIGNED_BYTE,image);
       // void gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 1024, 512, 0, gl.RGB, gl.UNSIGNED_BYTE, ArrayBufferView? pixels);
@@ -339,20 +281,74 @@ function draw(gl, indices_length, vertex_len, u_ModelViewMatrix, pos_len) {
       //将0号纹理传递给着色器
       gl.uniform1i(u_Sampler,0);
 
+
+      /********************************配置光照参数***************************/
+      // Pass the light position into the shader.
+      let LightPosition = gl.getUniformLocation(gl.program, 'LightPosition');
+      gl.uniform4fv(LightPosition, [-50.0, 50.0, 50.0, 1.0]);
+
+      // 材质漫反射颜色参数 传到着色器
+      let Kd = gl.getUniformLocation(gl.program, 'Kd');
+      gl.uniform3fv(Kd, [1.0, 1.0, 1.0]);
+
+      // 光照漫反射参数
+      let Ld = gl.getUniformLocation(gl.program, 'Ld');
+      gl.uniform3fv(Ld, [1.0, 1.0, 1.0]);
+
+
+
+      /********************************配置坐标参数***************************/
+      var viewMatrix = new Matrix4();
+      // (视点，观察目标点，上方向)
+      //  setLookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ)
+      //664
+      viewMatrix.setLookAt(5,3,5,-0.5,0,0,0,1,0);///12,3,7,-0.5,0,0,0,1,0
+
+      var modelMatrix = new Matrix4();
+      modelMatrix.setRotate(rotateAdd, 0, 1, 0);
+      //设置模型矩阵的相关信息
+      // var modelMatrix = new Matrix4();
+      // modelMatrix.setRotate(0, 0, 5, 1);
+      // modelMatrix.setTranslate(1, 0, 0);
+      //console.log(modelMatrix);
+
+      //设置透视投影矩阵
+      var projMatrix = new Matrix4();
+      projMatrix.setPerspective(20, canvas.width/canvas.height, 1, 1000.0);
+
+      //计算出模型视图矩阵 viewMatrix.multiply(modelMatrix)相当于在着色器里面u_ViewMatrix * u_ModelMatrix
+      var modeViewProjectMatrix = projMatrix.multiply(viewMatrix.multiply(modelMatrix));
+
+      // Pass the modelView matrix into the shader.
+      let ModelViewMatrix = gl.getUniformLocation(gl.program, 'ModelViewMatrix');
+      gl.uniformMatrix4fv(ModelViewMatrix, false, viewMatrix.elements);
+
+      // Pass the mvp matrix into the shader.
+      let MVP = gl.getUniformLocation(gl.program, 'MVP');
+      gl.uniformMatrix4fv(MVP, false, modeViewProjectMatrix.elements);
+
+
+
+
+
+      // Clear & draw.
+      // gl.clearColor(0.3, 0.3, 0.3, 1.0);
+      
+
+
       // //绘制
       gl.clearColor(0.8,0.8,0.8,1);
 
-      gl.clear(gl.COLOR_BUFFER_BIT);
-      // gl.drawArrays(gl.TRIANGLE_STRIP,0,n);
-      // gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
-
+      // gl.clear(gl.COLOR_BUFFER_BIT);
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+      
       //n为索引的数量
-      gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_SHORT, 0);
+      gl.drawElements(gl.TRIANGLES, indices_len, gl.UNSIGNED_SHORT, 0);
+      
+      // if (rotateAdd%180 == 0)
+      //    console.log(rotateAdd);
+      
+      requestAnimationFrame(loadTexture);
+      rotateAdd += 0.5;
       
   }
-
-
-
-
-
-
